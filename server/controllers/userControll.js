fbf39f20f7bbe6b38da5e3fa7c8d8a7219e32cde
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-06 14:56:46 +0500

    error middleware

--- server/controllers/userControll.js
+++ server/controllers/userControll.js
@@ -1,7 +1,23 @@
+/**
+* @desc:    Yangi foydalanuvchini ro'yxatdan o'tkazish
+* @route:   /api/users/sign-up
+* @access:  Ommaviy
+**/
 const signUp = (req, res) => {
-    res.send('register')
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error("Iltimos barcha maydonni to'ldiring")
+    }
+
+    return res.json({name, email, password})
 }
 
+/**
+ * @desc:    Yangi foydalanuvchini ro'yxatdan o'tkazish
+ * @route:   /api/users/sign-in
+ * @access:  Ommaviy
+ **/
 const signIn = (req, res) => {
     res.send('Login')
 }
--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import { config } from "dotenv";
 import userRoute from "./routes/userRoute.js";
+import {errorHandler} from "./middleware/errorMiddleware.js";
 
 const app = express();
 config();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.send('okay')
 })
 
 app.use('/api/users', userRoute)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
