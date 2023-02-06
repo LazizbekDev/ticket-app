b0f1c13e3597040589a1d5f3ef86ec5cae34cc42
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-06 15:01:04 +0500

    error middleware

--- server/controllers/userControll.js
+++ server/controllers/userControll.js
@@ -1,9 +1,11 @@
+import asyncHandler from "express-async-handler"
+
 /**
 * @desc:    Yangi foydalanuvchini ro'yxatdan o'tkazish
 * @route:   /api/users/sign-up
 * @access:  Ommaviy
 **/
-const signUp = (req, res) => {
+const signUp = asyncHandler(async  (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
         res.status(400)
@@ -11,16 +13,16 @@ const signUp = (req, res) => {
     }
 
     return res.json({name, email, password})
-}
+})
 
 /**
  * @desc:    Yangi foydalanuvchini ro'yxatdan o'tkazish
  * @route:   /api/users/sign-in
  * @access:  Ommaviy
  **/
-const signIn = (req, res) => {
+const signIn = asyncHandler(async (req, res) => {
     res.send('Login')
-}
+})
 
 export {
     signUp,
