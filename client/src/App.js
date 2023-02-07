4e6a4711a6a22a63597199f66757e741f2e18636
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-07 18:41:36 +0500

    routeing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      saassa
-    </div>
+    <>
+        <BrowserRouter>
+            <div className={'container'}>
+                <Header />
+                <Routes>
+                    <Route path={'/'} element={<Home />}/>
+                    <Route path={'/sign-in'} element={<Login />}/>
+                    <Route path={'/sign-up'} element={<Login register={true} />}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    </>
   );
 }
 
