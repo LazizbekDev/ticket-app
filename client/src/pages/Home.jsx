06568984166af19f0b0a5fd6c9f2b8d03c72253c
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-09 17:15:16 +0500

    initial home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <section className={'heading'}>
+                <h1>Sizga qanday yordam kerak?</h1>
+                <p>quyidagi variantdan tanlang</p>
+            </section>
+
+            <Link to={'/yangi-chipta'} className={'btn btn-reverse btn-block'}>
+                <FaQuestionCircle /> Yangi chipta yaratish
+            </Link>
+            <Link to={'/chiptalarim'} className={'btn btn-block'}>
+                <FaTicketAlt /> Chiptalarimni ko'rish
+            </Link>
+        </>
     );
 };
 
