bb2e58b3429ac259b07d008d4f0f96384faf056f
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-09 18:54:12 +0500

    multi languages

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import {initReactI18next} from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        supportedLngs: ['uz', 'en'],
+        fallbackLng: 'en',
+        debug: true,
+        detection: {
+            order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+            caches: ['cookie']
+        },
+        backend: {
+            loadPath: "/languages/{{lng}}/translation.json"
+        },
+        react: {
+            useSuspense: false
+        }
+    });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
 import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
-
+import {useTranslation} from "react-i18next";
+import i18next from "i18next";
 const Home = () => {
+
+    const { t } = useTranslation();
+
     return (
         <>
             <section className={'heading'}>
-                <h1>Sizga qanday yordam kerak?</h1>
-                <p>quyidagi variantdan tanlang</p>
+                <h1>{t('yordam')}</h1>
+                <p>{t('variant')}</p>
             </section>
-
             <Link to={'/yangi-chipta'} className={'btn btn-reverse btn-block'}>
                 <FaQuestionCircle /> Yangi chipta yaratish
             </Link>
+
+            <select onChange={(e) => i18next.changeLanguage(e.target.value)}>
+                <option value={'uz'}>UZ</option>
+                <option value={'en'}>En</option>
+            </select>
+
             <Link to={'/chiptalarim'} className={'btn btn-block'}>
                 <FaTicketAlt /> Chiptalarimni ko'rish
             </Link>
