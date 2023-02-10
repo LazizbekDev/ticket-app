d2f105ce6eacb11fc1658438ab69cffffb43ef6c
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-10 12:34:53 +0500

    new package

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,41 @@
 import { FaUser, FaSignInAlt } from "react-icons/fa";
+import { IoLanguage } from "react-icons/io5"
 import { Link } from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import i18next from "i18next";
+import jsCookie from "js-cookie";
+import Select from 'react-select'
 
 const Header = () => {
     const { t } = useTranslation();
+    console.log(jsCookie.get('i18next'))
+    const options = [
+        {
+            value: 'uz',
+            label: "O'Z",
+            disabled: 'uz' === jsCookie.get('i18next')
+        },
+        {
+            value: 'en',
+            label: 'Eng',
+            disabled: 'en' === jsCookie.get('i18next')
+        }
+    ]
+
+    const { label } = options.find(op => op.value === 'uz')
+
+    const colourStyles = {
+        option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+            // const color = chroma(data.color);
+            console.log({ data, isDisabled, isFocused, isSelected });
+            return {
+                ...styles,
+                backgroundColor: isFocused ? "#000" : isDisabled && "#d0cccc",
+                color: isFocused ? "#fff" : isDisabled ? "#313131" : "#000"
+            };
+        }
+    };
+
     return (
         <header className={'header'}>
             <div className={'logo'}>
@@ -22,13 +53,18 @@ const Header = () => {
                     </Link>
                 </li>
                 <li>
-                    <select
-                        style={{marginBottom: 0}}
-                        className={'btn btn-block'}
-                        onChange={(e) => i18next.changeLanguage(e.target.value)}>
-                        <option value={'uz'}>UZ</option>
-                        <option value={'en'}>En</option>
-                    </select>
+
+                    <Select
+                        isSearchable={false}
+                        placeholder={<span className={'flex-center'}>
+                            <IoLanguage size={'1rem'} /> {"  " + label}
+                        </span>}
+                        defaultValue={jsCookie.get('i18next')}
+                        isOptionDisabled={(opt) => opt.disabled}
+                        options={options}
+                        styles={colourStyles}
+                        onChange={(e) => i18next.changeLanguage(e.value)}
+                    />
                 </li>
             </ul>
         </header>
