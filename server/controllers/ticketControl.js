aa4a730aac48417247df46c7a6d01e72af9ecd41
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-13 15:07:41 +0500

    create ticket

--- server/controllers/ticketControl.js
+++ server/controllers/ticketControl.js
@@ -25,7 +25,28 @@ const getTickets = asyncHandler(async (req, res) => {
  * @access:  Shaxsiy
  **/
 const createTicket = asyncHandler(async (req, res) => {
-    return res.status(200).send({message: "I love? POST REQUEST"})
+    const { product, description } = req.body;
+
+    if (!product || !description) {
+        res.status(400)
+        throw new Error('Mahsulot va tavsifni kiriting!')
+    }
+
+    const user = await User.findOne(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('Foydalanuvchi topilmadi!')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: "new"
+    })
+
+    return res.status(201).send(ticket)
 })
 
 export {
