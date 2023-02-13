9d106fe619c8c61a0d8aa58486778899c54f9139
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-13 15:07:24 +0500

    get ticket

--- server/controllers/ticketControl.js
+++ server/controllers/ticketControl.js
@@ -1,4 +1,6 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import Ticket from "../models/ticketModel.js"
 
 /**
  * @desc:    Foydalanuchi chiptalarini olish
@@ -6,7 +8,15 @@ import asyncHandler from "express-async-handler";
  * @access:  Shaxsiy
  **/
 const getTickets = asyncHandler(async (req, res) => {
-    return res.status(200).send({message: "I love?"})
+    const user = await User.findOne(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('Foydalanuvchi topilmadi!')
+    }
+
+    const tickets = await Ticket.find({ user: req.user.id})
+    return res.status(200).json(tickets)
 })
 
 /**
