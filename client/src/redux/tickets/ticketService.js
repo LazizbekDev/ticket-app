dcb509108451142e98dba2df3f85d860c8d90504
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-18 15:23:12 +0500

    ticket service

---
+++ client/src/redux/tickets/ticketService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import {toast} from "react-toastify";
+
+const API_URL = 'api/tickests'
+const create = async (ticketData, token) => {
+    const response = await axios.post(API_URL, ticketData, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    if (response.data && !response.data.message) {
+        return response.data
+    }
+
+    if (response.data.message) {
+        toast(response.data.message)
+    }
+}
+
+const ticketService = {
+    create
+}
+
+export default ticketService
