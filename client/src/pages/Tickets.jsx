9cf30eaed386bce892c7f24f5160ff62cc8a0703
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-22 23:11:25 +0500

    tickets on display

--- client/src/pages/Tickets.jsx
+++ client/src/pages/Tickets.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {getTickets, reset} from "../redux/tickets/ticketSlice";
 import Loader from "../components/Loader";
+import TicketItem from "../components/TicketItem";
 
 const Tickets = () => {
     const { tickets, isLoading, isSuccess } = useSelector((state) => state.ticket);
@@ -20,10 +21,21 @@ const Tickets = () => {
     }, [dispatch])
 
     return (
-        <div>
-            <h1>Tickets</h1>
+        <>
             {isLoading && <Loader />}
-        </div>
+            <h1>Tickets</h1>
+            <div className={'tickets'}>
+                <div className={'ticket-headings'}>
+                    <div>date</div>
+                    <div>Product</div>
+                    <div>Status</div>
+                    <div></div>
+                </div>
+                {tickets.map((ticket, index) => (
+                    <TicketItem ticket={ticket} key={index} />
+                ))}
+            </div>
+        </>
     );
 };
 
