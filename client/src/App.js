c67c27890063b34c3ae79fcfd9df20e7f08a1627
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-22 22:40:24 +0500

    ticket page private route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 import NewTicket from "./pages/NewTicket";
 import PrivateRoute from "./components/PrivateRoute";
+import Tickets from "./pages/Tickets";
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
                     <Route path={'/sign-up'} element={<Login register={true} />}/>
                     <Route path={'/yangi-chipta'} element={<PrivateRoute />}>
                         <Route path={'/yangi-chipta'} element={<NewTicket />}/>
+                    </Route>
+                    <Route path={'/chiptalarim'} element={<PrivateRoute />}>
+                        <Route path={'/chiptalarim'} element={<Tickets />}/>
                     </Route>
                 </Routes>
             </div>
