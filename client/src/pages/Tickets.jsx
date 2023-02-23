72c30be4db3b237891976244fca5f1a520b27da9
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2023-02-23 12:57:01 +0500

    tickets status display features

--- client/src/pages/Tickets.jsx
+++ client/src/pages/Tickets.jsx
@@ -3,10 +3,12 @@ import {useDispatch, useSelector} from "react-redux";
 import {getTickets, reset} from "../redux/tickets/ticketSlice";
 import Loader from "../components/Loader";
 import TicketItem from "../components/TicketItem";
+import {useTranslation} from "react-i18next";
 
 const Tickets = () => {
     const { tickets, isLoading, isSuccess } = useSelector((state) => state.ticket);
     const dispatch = useDispatch();
+    const { t } = useTranslation();
 
     useEffect(() => {
         return () => {
@@ -26,13 +28,17 @@ const Tickets = () => {
             <h1>Tickets</h1>
             <div className={'tickets'}>
                 <div className={'ticket-headings'}>
-                    <div>date</div>
-                    <div>Product</div>
-                    <div>Status</div>
+                    <div>{t('new_ticket.date')}</div>
+                    <div>{t('new_ticket.product')}</div>
+                    <div>{t('new_ticket.status')}</div>
                     <div></div>
                 </div>
                 {tickets.map((ticket, index) => (
-                    <TicketItem ticket={ticket} key={index} />
+                    <TicketItem
+                        ticket={ticket}
+                        action={t('button.view')}
+                        key={index}
+                    />
                 ))}
             </div>
         </>
